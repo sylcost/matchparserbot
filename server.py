d948df6ca0a062ffdffdb8db4a5f0946aae58fa3
sylcost <37152750+sylcost@users.noreply.github.com>
2018-10-10 17:59:06 +0200

    add coorectdb()

--- server.py
+++ server.py
@@ -15,48 +15,57 @@ import datetime
     #https://www.youtube.com/watch?v=60WNEb6dHsM   acho
 
 app = Flask(__name__)
+
+# init log levels
 logLevels = {"DEBUG":10, "INFO":20, "WARNING":30, "ERROR":40,"CRITICAL":50}
+# defualt log level
 level = logLevels["INFO"]
 
+# read conf
 Config = ConfigParser.ConfigParser()
 Config.read("conf.ini")
-levelConf = Config.get('LOG', 'level')
 
+# get log level from conf if specified
+levelConf = Config.get('LOG', 'level')
 if levelConf != None and logLevels[levelConf] != None:
     level = logLevels[levelConf]
 
-print level
-#logger
+# loggers
 log = logging.getLogger('apscheduler.executors.default')
 log2 = logging.getLogger('apscheduler.scheduler')
 log.setLevel(level)
 log2.setLevel(level)
-
 fmt = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
 h = logging.StreamHandler()
 h.setFormatter(fmt)
 log.addHandler(h)
 log2.addHandler(h)
 
+# get mongo conf
 if Config.get('DB', 'mongoHost') == "" or Config.get('DB', 'mongoPort') == "" or Config.get('DB', 'mongoDbName') == "":
-    print("please configure mongo connection")
+    print("please configure mongo connection in conf.ini")
     sys.exit()
 
 #mongodb
 app.config["MONGO_URI"] = "mongodb://"+Config.get('DB', 'mongoHost')+":"+Config.get('DB', 'mongoPort')+"/"+Config.get('DB', 'mongoDbName')
 mongo = PyMongo(app)
 
+# objects doing the work
 downloader = Downloader(mongo)
 parser = Parser(mongo)
-
 scheduler = None
 
-
+# fire the main job
 @app.route('/firemainjob')
 def index():
     print("fire scheduler")
     scheduler.get_job("mainJob").modify(next_run_time=datetime.now())
 
+
+# Launched  by scheduler
+# Get next video to DL from DB
+# Download it then parse it
+# Repeat
 def work():
     print("work")
 
@@ -70,7 +79,7 @@ def work():
 
     print("fin work")
 
-
+# get the next video id to download
 def getNextVideoId():
     print("getnextvideo")
     id = ""
@@ -81,14 +90,32 @@ def getNextVideoId():
     print("_id:"+id)
     return id
 
+# In case the process has been shutdown during download or parsing, set video to redo the interrupted process
+def correctDB():
+    print("correct DB")
 
+    mongo.db.videos.update({
+        "status": "DOWNLOADING"
+    }, {
+        "$set": {
+            "status": "WAITING",
+            "dl": 0
+    }})
+    mongo.db.videos.update({
+        "status": "PARSING"
+    }, {
+        "$set": {
+            "status": "DOWNLOADED",
+            "frameCounter": 0
+     }})
 
 
 if __name__ == '__main__':
     global scheduler
     scheduler = BackgroundScheduler()
+    correctDB()
 
-    scheduler.add_job(work, 'interval', seconds=10, id="mainJob", misfire_grace_time=None)
-    #scheduler.start()
+    scheduler.add_job(work, 'interval', hours=24, id="mainJob", misfire_grace_time=None)
+    scheduler.start()
 
 app.run()
