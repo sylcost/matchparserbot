819bfab1c549facffd265b57512d2f0b1ddfbab2
sylcost <37152750+sylcost@users.noreply.github.com>
2018-11-23 01:34:27 +0100

    add try/catch + cleaning

--- serveur.js
+++ serveur.js
@@ -38,15 +38,14 @@ const schemaVideo = mongoose.Schema({
     "addDate": String,
     "publishedDate": String,
     "dl": Number,
-    "frameTotal": Number,
-    "frameCounter": Number,
-    "_id": String,
-    "text": String
+    "parsing": Number,
+    "_id": String
 });
 const Video = mongoose.model('Video', schemaVideo);
 
 // Creation Schema Match
 const schemaMatch = mongoose.Schema({
+    "_id": String,
     "idVideo": String,
     "hour": String,
     "minute": String,
@@ -58,7 +57,7 @@ const Match = mongoose.model('Match', schemaMatch);
 
 // Front.
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html')
 });
 
 
@@ -71,39 +70,62 @@ app.get('/checkvideo/:url', async function(req, res) {
 
 app.get('/addvideo/:url', async function(req, res) {
     console.log(req.params.url)
-    let video = await checkVideo(req.params.url)
-    if (!video.rejectCode) {
-        await addVideo(video)
+    let video = {}
+    try {
+        video = await checkVideo(req.params.url)
+        if (!video.rejectCode) {
+            video = await addVideo(video)
+            if (!video.error) {
+                // Video has been successfully added
+                video = {...video, "rejectCode": 1}
+            } // TODO si l'ajout a eu une erreur, faire un truc
+        }
+    } 
+    catch (err) {
+        console.log(`Error addVideo : ${err}`)
     }
-    console.log("result:"+JSON.stringify(result))
-    res.send(result)
+
+    // Call python server to start the download/parsing job
+    try {
+        await fetch("http://localhost:5000/firemainjob")
+    } catch (err) {
+        // don't care, video has been added to db anyway
+        console.log(`Error calling python server /firemainjob : ${err}`)
+    }
+    
+    res.send(video)
 });
 
 
 checkVideo = async (url) => {
-    let result;
-    let valid = ytdl.validateURL('http://www.youtube.com/watch?v='+url)
-    if (valid) {
-        let result = await ytdl.getBasicInfo('http://www.youtube.com/watch?v='+url)
-        let publishedDate = await getPublishedDate(result.video_id)
-        const videoDB = await getVideoFromDB(result.video_id)
-        
-        let videoInfo = {
-            "thumbnail": result.thumbnail_url,
-            "title": result.title,
-            "channelName": result.author.name,
-            "channelUrl": result.author.channel_url,
-            "length": formatLength(result.length_seconds),
-            "url": result.video_url,
-            "publishedDate": publishedDate,
-            "_id": result.video_id
-        }
-        let rejectGameACHO = "GAMEacho" !== result.author.name ? {"rejectCode": 5} : {}
-        let rejectName = result.title.indexOf("BBCF") > 0 || result.title.indexOf("BLAZBLUE") > 0 ? {} : {"rejectCode": 3}
-        return {...videoInfo, ...rejectGameACHO, ...rejectName, ...videoDB}
 
-    } else {
-        return {"rejectCode": 2}
+    try {
+        let valid = ytdl.validateURL('http://www.youtube.com/watch?v='+url)
+        if (valid) {
+            let result = await ytdl.getBasicInfo('http://www.youtube.com/watch?v='+url)
+            let publishedDate = await getPublishedDate(result.video_id)
+            const videoDB = await getVideoFromDB(result.video_id)
+            
+            let videoInfo = {
+                "thumbnail": result.thumbnail_url,
+                "title": result.title,
+                "channelName": result.author.name,
+                "channelUrl": result.author.channel_url,
+                "length": formatLength(result.length_seconds),
+                "url": result.video_url,
+                "publishedDate": publishedDate,
+                "_id": result.video_id
+            }
+            let rejectGameACHO = "GAMEacho" !== result.author.name ? {"rejectCode": 5} : {}
+            let rejectName = result.title.indexOf("BBCF") > 0 || result.title.indexOf("BLAZBLUE") > 0 ? {} : {"rejectCode": 3}
+            return {...videoInfo, ...rejectGameACHO, ...rejectName, ...videoDB}
+    
+        } else {
+            return {"rejectCode": 2}
+        }
+    }
+    catch (err) {
+        console.log(`Error during checkVideo() : ${err}`)
     }
 }
 
@@ -113,9 +135,6 @@ formatLength = (timeInSeconds) => {
     let seconds = timeInSeconds % 60
     let minutes = ((timeInSeconds - seconds) / 60) % 60
     let hours = (((timeInSeconds - seconds) / 60) - minutes) / 60
-    // console.log("seconds:"+seconds)
-    // console.log("minutes:"+minutes)
-    // console.log("hours:"+hours)
     let length = "";
     if (hours !== 0) {
         length += hours+"h"
@@ -133,24 +152,40 @@ getPublishedDate = async (id) => {
     let result = "";
     const url = "https://www.googleapis.com/youtube/v3/videos?id="+id+"&key="+SECRET_KEY+"&part=snippet"
 
-    let data = await fetch(url)
-    let json = await data.json()
-    if (json.items && json.items.length > 0) {
-        result = json.items[0].snippet.publishedAt
+    try {
+        let data = await fetch(url)
+        let json = await data.json()
+        if (json.items && json.items.length > 0) {
+            result = json.items[0].snippet.publishedAt
+        }
     }
-
+    catch (err) {
+        console.log(`Error during getPublishedDate() : ${err}`)
+    }
+    
     return result
 }
 
 getVideoFromDB = async (id) => {
     let result = {}
-    const video = await Video.findById(id)
-    if (video) {
-        result = {
-            "rejectCode": 1,
-            "text": video.text
+    let matches = []
+    try {
+        const video = await Video.findById(id)
+        if (video) {
+    
+            if (video.status === "FINISHED") {
+                matches = await Match.find({"idVideo": id })
+            }
+            result = {
+                "rejectCode": 1,
+                "matches": matches
+            }
         }
+    } 
+    catch (err) {
+        console.log(`Error during getVideoFromDB() : ${err}`)
     }
+    
     return result
 }
 
@@ -162,42 +197,48 @@ addVideo = async (vid) => {
         "addDate": "dateajout", 
         "dl": 0,
         "frameCounter": 0,
-        "frameTotal": 0,
-        "text": ""
+        "frameTotal": 0
     }
     try {
         await Video.create(video)
     } catch (err) {
         console.log(`error addvideo: ${err}`)
-        result = {
-            "rejectCode": 0
+        video = {
+            "error": `error addvideo: ${err}`
         }
     }
     
-    return {...video, ...result}
+    return video
 }
 
 
 checkYoutubeChannelForNewVideos = async () => {
+
     const GAMEachoId = "UCCfnriDcUslGMUMX4Ctkyjg"
     let result = "";
     const url = "https://www.googleapis.com/youtube/v3/activities?channelId="+GAMEachoId+"&maxResults=25&key="+SECRET_KEY+"&part=snippet,contentDetails"
 
-    let data = await fetch(url)
-    let json = await data.json()
+    try {
+        let data = await fetch(url)
+        let json = await data.json()
+        
+        //console.log(JSON.stringify(json))
+        console.log(" ")
+        if (json.items) {
     
-    //console.log(JSON.stringify(json))
-    console.log(" ")
-    if (json.items) {
-
-        const vids = json.items.filter(activity => {
-            return !!activity.contentDetails.upload && !!activity.contentDetails.upload.videoId
-        })
-
-        var results = await Promise.map(vids, async (video) => await checkVideo(video.contentDetails.upload.videoId));
-        results.filter(video => !video.rejectCode)
-        .forEach(video => addVideo(video))
-
+            const vids = json.items.filter(activity => {
+                return !!activity.contentDetails.upload && !!activity.contentDetails.upload.videoId
+            })
+    
+            var results = await Promise.map(vids, async (video) => await checkVideo(video.contentDetails.upload.videoId));
+            results
+            .filter(video => !video.rejectCode)
+            .forEach(video => addVideo(video))
+    
+        }
+    }
+    catch (err) {
+        console.log(`Error during checkYoutubeChannelForNewVideos() : ${err}`)
     }
 
     return result
@@ -218,8 +259,6 @@ io.on('connection', (socket) => {
 schedulerWs = async () => {
     if (Object.keys(io.sockets.sockets).length > 0) {
         try {
-
-
             const list = await Video.find({ status: { $ne: "FINISHED" }});
             const queued = list.map(videodb => {
                 return {
@@ -227,13 +266,11 @@ schedulerWs = async () => {
                     "title": videodb.title,
                     "dl": videodb.dl,
                     "addDate": videodb.addDate,
-                    "frameTotal": videodb.frameTotal,
-                    "frameCounter": videodb.frameCounter,
+                    "parsing": videodb.parsing,
                     "publishedDate": videodb.publishedDate,
                     "length": videodb.length,
                     "channelName": videodb.channelName,
                     "channelUrl": videodb.channelUrl,
-                    "text": videodb.text,
                     "status": videodb.status,
                     "thumbnail": videodb.thumbnail
                 }
@@ -247,7 +284,7 @@ schedulerWs = async () => {
 }
 
 // Lancement du Job WS toutes les 10s.
-cron.schedule('*/10 * * * * *', schedulerWs);
-cron.schedule('0 * * * * *', checkYoutubeChannelForNewVideos);
+cron.schedule('*/5 * * * * *', schedulerWs);
+//cron.schedule('0 * * * * *', checkYoutubeChannelForNewVideos);
 
 
